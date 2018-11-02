e75fcda5e22aa630b8cf076054e811b3dde73ce8
yozaam <yozaam@gmail.com>
2018-11-02 13:00:46 +0530

    test to understand .wasm

---
+++ src/example.cpp
@@ -0,0 +1,9 @@
+int squarer(int num) {
+  return num * num;
+}
+//inserting this function code in https://mbebenita.github.io/WasmExplorer/ 
+
+//it will return a wat with a line like this
+// (export "_Z7squareri" (func $_Z7squareri))
+// this is now the function name "_Z7squareri" that we use through javascript
+// download the .wasm and put it alongside an index.html to access the function
